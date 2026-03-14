4b81cef37e35e7247149acf68735a934a3342f36
Douglas C. Ferreira <douglascf12@live.com>
2026-03-14 12:30:14 -0300

    refactor: melhora modelagem com optionals e adiciona mapper seguro de CoinDTO para Coin

--- PulseInvest/Presentation/Common/AppCoordinator.swift
+++ PulseInvest/Presentation/Common/AppCoordinator.swift
@@ -9,6 +9,29 @@ final class AppCoordinator {
     }
     
     func start() {
+        let sampleDTO = CoinDTO(
+            id: "bitcoin",
+            symbol: "btc",
+            name: "Bitcoin",
+            image: "https://assets.coingecko.com/coins/images/1/large/bitcoin.png",
+            currentPrice: 65000
+        )
+        
+        if let coin = CoinMapper.map(dto: sampleDTO) {
+            print("Mapped coin:", coin)
+        }
+        
+        let invalidDTO = CoinDTO(
+            id: nil,
+            symbol: "eth",
+            name: "Ethereum",
+            image: nil,
+            currentPrice: nil
+        )
+        
+        let invalidCoin = CoinMapper.map(dto: invalidDTO)
+        print("Invalid mapped coin:", invalidCoin as Any)
+        
         let viewController = UIViewController()
         viewController.view.backgroundColor = .systemBackground
         viewController.title = "PulseInvest"
